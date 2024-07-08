58556e9ae691eacfb769c71a7c317bf5b7e42915
Vaaargr <Antnsapstray@mail.ru>
2024-07-08 17:44:46 +0300

    Создание графа навигации, всех основных фрагментов и добавление navControllera в RootActivity

---
+++ app/src/main/java/ru/practicum/android/diploma/favourite/ui/FavouriteFragment.kt
@@ -0,0 +1,18 @@
+package ru.practicum.android.diploma.favourite.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.databinding.FavouriteFragmentBinding
+
+class FavouriteFragment: Fragment() {
+    private var _binding: FavouriteFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FavouriteFragmentBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
---
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/ui/VacancyFragment.kt
@@ -0,0 +1,20 @@
+package ru.practicum.android.diploma.vacancy.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.databinding.VacancyFragmentBinding
+
+class VacancyFragment : Fragment() {
+
+    private var _binding: VacancyFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = VacancyFragmentBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+}
