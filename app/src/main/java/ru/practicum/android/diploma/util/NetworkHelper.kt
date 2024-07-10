30f76d0a9d9df94196ce959a45113e27820070b7
Krokodil_Gena <alex.usevich@gmail.com>
2024-07-10 15:50:34 +0300

    Fixed warnings

--- app/src/main/java/ru/practicum/android/diploma/util/NetworkHelper.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/NetworkHelper.kt
@@ -3,13 +3,10 @@ package ru.practicum.android.diploma.util
 import android.content.Context
 import android.net.ConnectivityManager
 
-class NetworkHelper {
-    companion object {
-        fun isOnline(context: Context): Boolean {
-            val connectivityManager =
-                context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
-            return connectivityManager != null
-        }
+object NetworkHelper {
+    fun isOnline(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
+        return connectivityManager != null
     }
-
 }
