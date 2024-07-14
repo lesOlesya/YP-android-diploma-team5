65fdf646fa4d5b3ee65a83e5e5e64820e8360a10
Krokodil_Gena <alex.usevich@gmail.com>
2024-07-15 02:20:05 +0300

    Completed task

---
+++ app/src/main/java/ru/practicum/android/diploma/favourite/ui/FavoritesViewModel.kt
@@ -0,0 +1,6 @@
+package ru.practicum.android.diploma.favourite.ui
+
+import androidx.lifecycle.ViewModel
+
+class FavoritesViewModel: ViewModel() {
+}
--- app/src/main/java/ru/practicum/android/diploma/favourite/ui/FavouriteFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/favourite/ui/FavouriteFragment.kt
@@ -6,10 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ru.practicum.android.diploma.databinding.FavouriteFragmentBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class FavouriteFragment : Fragment() {
     private var _binding: FavouriteFragmentBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: FavoritesViewModel by viewModel()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FavouriteFragmentBinding.inflate(layoutInflater, container, false)
