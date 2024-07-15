d9886d9198ffdf7ee9c5944dcfa2b8b18c3b9883
Krokodil_Gena <alex.usevich@gmail.com>
2024-07-16 00:31:49 +0300

    Completed task

--- app/src/main/java/ru/practicum/android/diploma/search/domain/SearchRepository.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/domain/SearchRepository.kt
@@ -1,10 +1,9 @@
 package ru.practicum.android.diploma.search.domain
 
 import kotlinx.coroutines.flow.Flow
-import ru.practicum.android.diploma.search.data.dto.VacancyDto
 import ru.practicum.android.diploma.search.data.dto.VacancySearchResponse
 import ru.practicum.android.diploma.util.Resource
 
 interface SearchRepository {
-    suspend fun search(text: String) : Flow<Resource<VacancySearchResponse>>
+    suspend fun search(text: String): Flow<Resource<VacancySearchResponse>>
 }
--- app/src/main/java/ru/practicum/android/diploma/search/domain/impl/SearchInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/domain/impl/SearchInteractorImpl.kt
@@ -1,12 +1,11 @@
 package ru.practicum.android.diploma.search.domain.impl
 
 import kotlinx.coroutines.flow.Flow
-import ru.practicum.android.diploma.search.data.dto.VacancyDto
 import ru.practicum.android.diploma.search.data.dto.VacancySearchResponse
 import ru.practicum.android.diploma.search.domain.SearchInteractor
 import ru.practicum.android.diploma.search.domain.SearchRepository
 import ru.practicum.android.diploma.util.Resource
 
-class SearchInteractorImpl(private val repository: SearchRepository) : SearchInteractor {
+class SearchInteractorImpl(private val repository: SearchRepository): SearchInteractor {
     override suspend fun search(text: String): Flow<Resource<VacancySearchResponse>> = repository.search(text)
 }
