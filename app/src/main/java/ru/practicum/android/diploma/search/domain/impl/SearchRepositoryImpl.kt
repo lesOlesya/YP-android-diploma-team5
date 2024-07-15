b1ef82075e7cdd7c25a801a4733e371f1f106ffa
Krokodil_Gena <alex.usevich@gmail.com>
2024-07-15 21:24:44 +0300

    Completed task

--- app/src/main/java/ru/practicum/android/diploma/search/domain/impl/SearchRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/domain/impl/SearchRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package ru.practicum.android.diploma.search.domain.impl
 
+import android.util.Log
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -8,18 +9,14 @@ import ru.practicum.android.diploma.search.data.dto.VacancySearchResponse
 import ru.practicum.android.diploma.search.data.network.HeadHunterApi
 import ru.practicum.android.diploma.search.domain.SearchRepository
 
-class SearchRepositoryImpl(private val retrofitService: HeadHunterApi): SearchRepository {
-    override suspend fun search(term: String): Flow<VacancySearchResponse> = flow  {
-    try {
-            val response: VacancySearchResponse = retrofitService.search(term)
+class SearchRepositoryImpl(private val retrofitService: HeadHunterApi) : SearchRepository {
+    override suspend fun search(term: String): Flow<VacancySearchResponse> = flow {
+        val response: VacancySearchResponse = retrofitService.search(term)
         if (response.resultCode == 0) {
             emit((response))
         } else {
-            //Обработать ошибку здесь нужно
+            // Обработать ошибку здесь нужно
             emit((response))
-        }
-        } catch (e: Exception) {
-           // emit(e.message ?: "An error occurred")
         }
     }.flowOn(Dispatchers.IO)
 
