d662bcd4c86120c01e2ac8ba2ce81b9c569e6104
LesOlesya <olesyad285@gmail.com>
2024-07-15 14:33:48 +0300

    headers api

--- app/src/main/java/ru/practicum/android/diploma/search/data/network/HeadHunterApi.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/network/HeadHunterApi.kt
@@ -1,7 +1,7 @@
 package ru.practicum.android.diploma.search.data.network
 
 import retrofit2.http.GET
-import retrofit2.http.Header
+import retrofit2.http.Headers
 import retrofit2.http.Path
 import retrofit2.http.Query
 import ru.practicum.android.diploma.BuildConfig
@@ -12,9 +12,12 @@ interface HeadHunterApi {
     @GET("/search")
     suspend fun search(@Query("text") text: String): VacancySearchResponse
 
+    // не уверена, правильно или нет
+    @Headers("Authorization: Bearer {token}",
+        "HH-User-Agent: Jobka (olesyad285@gmail.com)")
     @GET("/vacancies/{vacancy_id}")
     suspend fun getVacancyDetails(
-        @Header("Authorization") token: String = BuildConfig.HH_ACCESS_TOKEN,
+        @Path("token") token: String = BuildConfig.HH_ACCESS_TOKEN,
         @Path("vacancy_id") vacancyId: String
     ): VacancySearchResponse
 
