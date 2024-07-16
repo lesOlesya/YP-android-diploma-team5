7cafc7938df2b5d2c3eacf04dff59c321503b0bd
Krokodil_Gena <alex.usevich@gmail.com>
2024-07-16 07:47:52 +0300

    Completed task

--- app/src/main/java/ru/practicum/android/diploma/search/domain/impl/SearchInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/domain/impl/SearchInteractorImpl.kt
@@ -1,7 +1,6 @@
 package ru.practicum.android.diploma.search.domain.impl
 
 import kotlinx.coroutines.flow.Flow
-import ru.practicum.android.diploma.search.data.dto.VacancySearchResponse
 import ru.practicum.android.diploma.search.domain.SearchInteractor
 import ru.practicum.android.diploma.search.domain.SearchRepository
 import ru.practicum.android.diploma.search.domain.models.Vacancy
