67ce9627c912181131b74702e36efc8cd1994179
Krokodil_Gena <alex.usevich@gmail.com>
2024-07-17 20:17:04 +0300

    Revert "Completed task"
    This reverts commit e34485f0384762702369ed9f514667f494af0f29.

--- app/src/main/java/ru/practicum/android/diploma/util/NetworkHelper.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/NetworkHelper.kt
@@ -2,26 +2,11 @@ package ru.practicum.android.diploma.util
 
 import android.content.Context
 import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import android.os.Build
 
 object NetworkHelper {
     fun isOnline(context: Context): Boolean {
         val connectivityManager =
             context.getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
-        if (connectivityManager != null) {
-            val capabilities =
-                connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-            if (capabilities != null) {
-                if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
-                    return true
-                } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
-                    return true
-                } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
-                    return true
-                }
-            }
-        }
-        return false
+        return connectivityManager != null
     }
 }
