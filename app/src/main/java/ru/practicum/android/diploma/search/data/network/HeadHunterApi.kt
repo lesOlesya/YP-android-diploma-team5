e6b09a60f1bb9433915d029018cc5829082ca989
Vaaargr <Antnsapstray@mail.ru>
2024-07-17 13:30:57 +0300

    VacancyFragment, VacancyViewModel

--- app/src/main/java/ru/practicum/android/diploma/search/data/network/HeadHunterApi.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/network/HeadHunterApi.kt
@@ -4,7 +4,6 @@ import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Path
 import retrofit2.http.Query
-import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.search.data.dto.VacancySearchResponse
 import ru.practicum.android.diploma.vacancy.data.dto.VacancyDetailsResponse
 
