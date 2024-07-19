e293aa3e34ad0e585affdc71357bbaf141dfa424
LesOlesya <olesyad285@gmail.com>
2024-07-19 15:05:48 +0300

    detekt

--- app/src/main/java/ru/practicum/android/diploma/filter/country/ui/ChoosingCountryFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/country/ui/ChoosingCountryFragment.kt
@@ -1,16 +1,17 @@
 package ru.practicum.android.diploma.filter.country.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import ru.practicum.android.diploma.R
 
 class ChoosingCountryFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
