cda95fd4372e024b03daeec951bcfb255e6f2dc3
verman <reg@vrmn.ru>
2024-07-21 20:57:00 +0300

    битва с детект

--- app/src/main/java/ru/practicum/android/diploma/filter/industry/ui/ChoosingIndustryScreenState.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/industry/ui/ChoosingIndustryScreenState.kt
@@ -1,8 +1,8 @@
 package ru.practicum.android.diploma.filter.industry.ui
 
 import ru.practicum.android.diploma.filter.industry.domain.model.Industry
-sealed interface ChoosingIndustryScreenState {
 
+sealed interface ChoosingIndustryScreenState {
     data class IndustriesUploaded(
         val industries: ArrayList<Industry>,
         val selectedIndustryId: String,
