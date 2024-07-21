b43e6a8bb9ee0cd762cd42461b943eeb393a4bbe
verman <reg@vrmn.ru>
2024-07-21 16:50:32 +0300

    Отображение списка отраслей на экране «Отрасли»(Presentation, UI) #80 битва с детектом

--- app/src/main/java/ru/practicum/android/diploma/filter/industry/presentation/ChoosingIndustryViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/industry/presentation/ChoosingIndustryViewModel.kt
@@ -1,15 +1,5 @@
 package ru.practicum.android.diploma.filter.industry.presentation
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import ru.practicum.android.diploma.filter.industry.domain.IndustryInteractor
-import ru.practicum.android.diploma.filter.industry.domain.models.Industry
-import ru.practicum.android.diploma.filter.industry.domain.models.ResponseStatus
-import ru.practicum.android.diploma.filter.industry.ui.ChoosingIndustryScreenState
 
 class ChoosingIndustryViewModel(
     private val industryInteractor: IndustryInteractor,
