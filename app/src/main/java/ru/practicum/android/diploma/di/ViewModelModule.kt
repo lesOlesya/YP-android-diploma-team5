dd3667bf52f1c16905b167ae15f17a0133ec64af
verman <reg@vrmn.ru>
2024-07-21 16:59:48 +0300

    Отображение списка отраслей на экране «Отрасли»(Presentation, UI) #80 как бы так закомплить..

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -3,6 +3,7 @@ package ru.practicum.android.diploma.di
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import ru.practicum.android.diploma.favourite.presentation.FavoritesViewModel
+import ru.practicum.android.diploma.filter.industry.presentation.ChoosingIndustryViewModel
 import ru.practicum.android.diploma.search.presentation.SearchViewModel
 import ru.practicum.android.diploma.vacancy.presentation.VacancyViewModel
 
@@ -17,5 +18,11 @@ val viewModelModule = module {
     }
     viewModel {
         VacancyViewModel(get(), get())
+    }
+
+    viewModel {
+        ChoosingIndustryViewModel(
+            industryInteractor = get(),
+        )
     }
 }
