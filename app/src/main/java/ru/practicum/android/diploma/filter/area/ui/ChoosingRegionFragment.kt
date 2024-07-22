57db33c1b1c428a90e5e56f716beae96796c15e3
Vaaargr <Antnsapstray@mail.ru>
2024-07-22 19:20:15 +0300

    detected

--- app/src/main/java/ru/practicum/android/diploma/filter/area/ui/ChoosingRegionFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/area/ui/ChoosingRegionFragment.kt
@@ -69,10 +69,11 @@ class ChoosingRegionFragment : Fragment(), AreaAdapter.AreaClickListener {
 
             editTextFilter.addTextChangedListener(object : TextWatcher {
                 override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
+                    //detect
                 }
-                override fun afterTextChanged(s: Editable?) {
 
+                override fun afterTextChanged(s: Editable?) {
+                    //detect
                 }
 
                 override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
