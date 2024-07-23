d2dd9ba24d953125e18ce898b908d3117b29614d
LesOlesya <olesyad285@gmail.com>
2024-07-23 16:01:41 +0300

    удалила ненужные файлы

--- app/src/main/java/ru/practicum/android/diploma/filter/country/domain/models/AreasInteractor.kt
+++
@@ -1,7 +0,0 @@
-package ru.practicum.android.diploma.filter.country.domain.models
-
-import kotlinx.coroutines.flow.Flow
-
-interface AreasInteractor {
-    fun getAreas(): Flow<AreasSearchResult>
-}
--- app/src/main/java/ru/practicum/android/diploma/filter/country/domain/models/ResponseStatus.kt
+++
@@ -1,9 +0,0 @@
-package ru.practicum.android.diploma.filter.country.domain.models
-
-enum class ResponseStatus {
-    OK,
-    BAD,
-    NO_CONNECTION,
-    LOADING,
-    DEFAULT
-}
