0a21361fdd935ffe281cc81ed4a223e675bc7103
Vaaargr <Antnsapstray@mail.ru>
2024-07-23 22:26:44 +0300

    Возвращение выбранной отросли

--- app/src/main/java/ru/practicum/android/diploma/filter/industry/presentation/ChoosingIndustryViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/industry/presentation/ChoosingIndustryViewModel.kt
@@ -8,18 +8,18 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import ru.practicum.android.diploma.filter.industry.domain.api.IndustryInteractor
 import ru.practicum.android.diploma.filter.industry.domain.model.Industry
+import ru.practicum.android.diploma.filter.settings.domain.api.FilterParametersInteractor
 import ru.practicum.android.diploma.util.Resource
 
 class ChoosingIndustryViewModel(
-    private val industryInteractor: IndustryInteractor
+    private val industryInteractor: IndustryInteractor,
+    private val filterInteractor: FilterParametersInteractor
 ) : ViewModel() {
 
     private val industries = ArrayList<Industry>()
 
     private val _choosingIndustryStateLiveData =
         MutableLiveData<ChoosingIndustryState>()
-    val choosingIndustryStateLiveData: LiveData<ChoosingIndustryState> =
-        _choosingIndustryStateLiveData
 
     init {
         getIndustries()
@@ -50,5 +50,20 @@ class ChoosingIndustryViewModel(
                 )
             )
         } ?: _choosingIndustryStateLiveData.postValue(ChoosingIndustryState.Error)
+    }
+
+    fun saveIndustryParameters(industry: Industry) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val parameters = filterInteractor.getParameters()
+            filterInteractor.saveParameters(
+                filterInteractor.buildFilterParameters(
+                    country = parameters.country,
+                    region = parameters.region,
+                    industry = industry,
+                    expectedSalary = parameters.expectedSalary,
+                    flagOnlyWithSalary = parameters.flagOnlyWithSalary
+                )
+            )
+        }
     }
 }
